5a3967f3eff4e8fd0e6833d289eb448ff890d118
Josh Minnich <joshminnich@gmail.com>
2012-08-09 11:50:29 -0700

    Added documentation to model and test file.

--- models/user.js
+++ models/user.js
@@ -1,12 +1,24 @@
+/*
+ * User Model
+ *
+ * Instead of following the traditional Mongoose examples, I'm
+ * using a function to provide both private and public methods to
+ * this model to keep things more organized!
+ */
+
 var User = function(){
   var mongoose = require('mongoose');
+  // Kinda redundant, but needed
   var Schema = require('mongoose').Schema;
+  // Mongoose schema so Mongoose can make effective queries
   var userSchema = new Schema({
     email : { type: String, index: { unique: true, required: true }},
     password: { type: String, required: true }
   });
 
+  // Declaring a private model for internal methods
   var _model = mongoose.model('user', userSchema);
+  // Creating a register method for convenience
   var _register = function(email, password, callback){
     _model.create({ email: email, password: password }, function(e, doc){
       if(e) {
@@ -16,6 +28,7 @@ var User = function(){
       }
     });
   };
+  // Creating a findByEmail method for convenience
   var _findByEmail = function(email, success, fail){
     _model.findOne({ email: email }, function(e, doc){
       if(e) {
@@ -26,6 +39,7 @@ var User = function(){
     });
   }
 
+  // Returning properties and methods we'd like to be public
   return {
     register: _register,
     schema: userSchema,
--- test/test.js
+++ test/test.js
@@ -1,10 +1,29 @@
 var mongoose = require('mongoose');
 var user = require('./../models/user');
 
+// Connecting to a local test database or creating it on the fly
 mongoose.connect('mongodb://localhost/user_test');
+
+/*
+ * Mocha Test
+ *
+ * Tests are organized by having a "describe" and "it" method. Describe
+ * basically creates a "section" that you are testing and the "it" method
+ * is what runs your test code.
+ *
+ * For asynchronous tests you need to have a done method that you call after
+ * your code should be done executing so Mocha runs to test properly.
+ */
 
 describe('Users', function(){
   var currentUser = null;
+
+  /*
+   * beforeEach Method
+   *
+   * The before each method will execute every time Mocha is run. This
+   * code will not run every time an individual test is run.
+   */
 
   beforeEach(function(done){
     user.register('test@test.com', 'password', function(doc){
@@ -12,6 +31,13 @@ describe('Users', function(){
       done();
     });
   });
+
+  /*
+   * afterEach Method
+   *
+   * Just like the beforeEach, afterEach is run after Mocha has completed
+   * running it's queue.
+   */
 
   afterEach(function(done){
     user.model.remove({}, function(){
